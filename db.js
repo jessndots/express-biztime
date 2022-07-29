6d22facdf2a82862c25cab4a72925487df05758e
jdots <jessndoty@gmail.com>
2022-07-29 08:23:33 -0700

    added routes

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 /** Database setup for BizTime. */
 
 const {Client} = require("pg");
+const password = require("./password")
 const DB_URI = (process.env.NODE_ENV === "test") ? "postgresql:///biztime_test": "postgresql:///biztime";
 let db = new Client({connectionString: DB_URI});
-db.password = 'secret';
+db.password = password;
 db.connect();
 
 module.exports = db;
